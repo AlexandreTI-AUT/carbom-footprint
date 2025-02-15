14956c8b2e71fdbf079feb1e64f557f36573a28b
Alexandre Silva <alexandre.silva@verifymy.io>
2025-02-15 13:05:31 -0300

    new version

--- src/FormCarbono.jsx
+++ src/FormCarbono.jsx
@@ -39,6 +39,8 @@ const FormCarbono = () => {
       setTipoEmissao("");
       setQuantidade("");
       setErrors({});
+
+      setTimeout(() => setSuccessMessage(""), 20000); // Expira após 20 segundos
     }
   };
 
@@ -65,12 +67,37 @@ const FormCarbono = () => {
 
         <div className="form-group">
           <label>Tipo de Emissão:</label>
-          <input
-            type="text"
+          <select
             value={tipoEmissao}
             onChange={(e) => setTipoEmissao(e.target.value)}
-            data-test-id="input-tipo-emissao"
-          />
+            data-test-id="select-tipo-emissao"
+          >
+            <option value="">Selecione um tipo</option>
+            <option value="CO₂ (Dióxido de Carbono)">
+              CO₂ (Dióxido de Carbono)
+            </option>
+            <option value="CH₄ (Metano)">CH₄ (Metano)</option>
+            <option value="N₂O (Óxido Nitroso)">N₂O (Óxido Nitroso)</option>
+            <option value="CFCs (Clorofluorcarbonetos)">
+              CFCs (Clorofluorcarbonetos)
+            </option>
+            <option value="HFCs (Hidrofluorcarbonetos)">
+              HFCs (Hidrofluorcarbonetos)
+            </option>
+            <option value="SF₆ (Hexafluoreto de Enxofre)">
+              SF₆ (Hexafluoreto de Enxofre)
+            </option>
+            <option value="O₃ (Ozônio)">O₃ (Ozônio)</option>
+            <option value="NOx (Óxidos de Nitrogênio)">
+              NOx (Óxidos de Nitrogênio)
+            </option>
+            <option value="SO₂ (Dióxido de Enxofre)">
+              SO₂ (Dióxido de Enxofre)
+            </option>
+            <option value="CO (Monóxido de Carbono)">
+              CO (Monóxido de Carbono)
+            </option>
+          </select>
           {errors.tipoEmissao && (
             <span className="error-message" data-test-id="erro-tipo-emissao">
               {errors.tipoEmissao}
@@ -99,7 +126,11 @@ const FormCarbono = () => {
       </form>
 
       {successMessage && (
-        <p className="success-message" data-test-id="mensagem-sucesso">
+        <p
+          className="success-message"
+          data-test-id="mensagem-sucesso"
+          style={{ color: "blue" }}
+        >
           {successMessage}
         </p>
       )}
